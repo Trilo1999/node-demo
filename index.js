a78a0afe4d7ac98eea2e1c17b0b5a26ada821888
t.r.i.l.o <trilochan.parida@ajatus.co.in>
2021-05-15 17:04:36 +0530

    hii

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var http = require("http");
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
- }).listen(8081);
- 
- // Console will print the message
- console.log('Server running at 8081');
+var express =require('express');
+var app =express();
+app.get("/users",function(req,res){
+    res.send("hiii users");
+})
+ app.listen(3030,()=>{
+     console.log("port started on 3030")
+ })
